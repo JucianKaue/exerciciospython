cb3c303916f30a3ab545e085214e689ec2ded39e
Jucian Decezare <jucian_decezare2015@hotmail.com>
2020-04-22 14:18:53 -0300

    Exercicios aula 13 não finalizado

--- ex046.py
+++ ex046.py
@@ -1,6 +1,6 @@
 from time import sleep
 import emoji
-for c in range(10,-1,-1):
+for c in range(10, -1, -1):
     print(c)
     sleep(1)
 print(emoji.emojize(':party_popper::party_popper: HAPPY NEW YEAR :party_popper::party_popper:'))
